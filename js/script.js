517d0d41308a8b44785c04da31700d4ab363f1cd
VolkovPavel <drkrain86@gmail.com>
2017-05-15 21:59:10 +0300

    Правки Б9,  Б12, Д3, Д8, Д13, Д14, Д15, Д17, Д19

--- js/script.js
+++ js/script.js
@@ -6,31 +6,36 @@ var userMail = document.querySelector("[name=email]");
 var form = document.querySelector(".write-us-form");
 var storage = localStorage.getItem("userName");
 
-formOpen.addEventListener("click", function(event){
-  event.preventDefault();
-  formPopup.classList.add("write-us-modal-show");
-  console.log("Форму открыть");
-  if (storage) {
-    userName.value = storage;
-    userMail.focus();
-  } else {
-  userName.focus();
-  }
-});
+if (formOpen) {
+  formOpen.addEventListener("click", function(event){
+    event.preventDefault();
+    formPopup.classList.add("write-us-modal-show");
+    console.log("Форму открыть");
+    if (storage) {
+      userName.value = storage;
+      userMail.focus();
+    } else {
+    userName.focus();
+    }
+  });
+}
 
-formClose.addEventListener("click", function(event){
-  event.preventDefault();
-  formPopup.classList.remove("write-us-modal-show");
-  formPopup.classList.remove("write-us-modal-error");
-  console.log("Форму закрыть");
+if (formClose) {
+  formClose.addEventListener("click", function(event){
+    event.preventDefault();
+    formPopup.classList.remove("write-us-modal-show");
+    formPopup.classList.remove("write-us-modal-error");
 });
+}
 
-form.addEventListener("submit",function(event){if (!userName.value || !userMail.value){event.preventDefault();
-  formPopup.classList.add("write-us-modal-error");
-  }
- else {localStorage.setItem("userName", userName.value);
-  }
-});
+if (form) {
+  form.addEventListener("submit",function(event){if (!userName.value || !userMail.value){event.preventDefault();
+    formPopup.classList.add("write-us-modal-error");
+    }
+   else {localStorage.setItem("userName", userName.value);
+    }
+  });
+}
 
 window.addEventListener("keydown",function(event){
   if(event.keyCode === 27) {
@@ -46,27 +51,34 @@ var mapOpen = document.querySelector(".map-open");
 var mapPopup = document.querySelector(".map-popup-modal");
 var mapClose = document.querySelector(".map-close");
 
+if (mapOpen) {
+  mapOpen.addEventListener("click", function (event){
+    event.preventDefault();
+    mapPopup.classList.add("map-modal-show");
+  });
+}
 
-mapOpen.addEventListener("click", function (event){
-  event.preventDefault();
-  mapPopup.classList.add("map-modal-show");
-});
+if (mapClose) {
+  mapClose.addEventListener("click", function(event){
+    event.preventDefault();
+    mapPopup.classList.remove("map-modal-show");
+  });
+}
 
-mapClose.addEventListener("click", function(event){
-  event.preventDefault();
-  mapPopup.classList.remove("map-modal-show");
-  console.log("Карту закрыть");
-});
-
-var cartBuy = document.querySelectorAll(".bookmark-btn");
+var cartBuy = document.querySelectorAll(".buy-btn");
 var cart = document.querySelector(".cart-modal");
 var cartClose = document.querySelector(".cart-close");
 
-cartBuy.eventTarget.addEventListener("click", function(event){event.preventDefault();
-  cart.classList.add("cart-modal-show");
-  console.log("Открыть окно");
-  });
+if (cartBuy) {
+  for (var i = 0; i < cartBuy.length; i++) {
+    cartBuy[i].addEventListener("click", function(event){event.preventDefault();
+      cart.classList.add("cart-modal-show");
+    });
+  }
+}
 
-cartClose.addEventListener("click", function(event){event.preventDefault();
-  cart.classList.remove("cart-modal-show");
-});
+if (cartClose) {
+  cartClose.addEventListener("click", function(event){event.preventDefault();
+    cart.classList.remove("cart-modal-show");
+  });
+}
